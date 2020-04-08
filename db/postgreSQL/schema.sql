00ecbcaea8b0d13fa7e2c34dc63e0ca47c8c1fbf
Joanna Wang <shiju.an362@gmail.com>
2020-04-08 15:31:51 -0700

    add psql connection to schema

--- db/postgreSQL/schema.sql
+++ db/postgreSQL/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE locations (
 );
 
 CREATE TABLE reservations (
-  reservation_id SERIAL PRIMARY KEY,
+  reservation_id INT PRIMARY KEY,
   checkin_date DATE NOT NULL,
   checkout_date DATE NOT NULL,
   adults INT,
@@ -29,7 +29,21 @@ CREATE TABLE reservations (
   infants INT,
   price INT,
   location_id INT,
-  user_id INT,
-  FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION,
-  FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION
+  user_id SERIAL
+  -- FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+  -- FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+COPY locations(location_id, location_address, rate, review_avg, total_review, service_fee, occupancy_tax) FROM '/Users/ohjeezz/Documents/HackReactor/pj3/reservation-services/db/postgreSQL/data/locations.csv' DELIMITER ',' CSV HEADER;
+
+COPY users(username, email) FROM '/Users/ohjeezz/Documents/HackReactor/pj3/reservation-services/db/postgreSQL/data/users.csv' DELIMITER ',' CSV HEADER;
+
+COPY reservations(reservation_id, checkin_date, checkout_date, adults, children, infants, price, location_id, user_id) FROM '/Users/ohjeezz/Documents/HackReactor/pj3/reservation-services/db/postgreSQL/data/reservations.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE reservations ADD CONSTRAINT fk_locations FOREIGN KEY (location_id) REFERENCES locations(listing_id);
+ALTER TABLE reservations ADD CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users(user_id);
+
+
+
+
+
