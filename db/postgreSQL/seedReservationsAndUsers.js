53934bbb1b97a8f6c5656c6ffe88bd903468e160
Joanna Wang <shiju.an362@gmail.com>
2020-04-07 20:54:43 -0700

    attempt drain method

--- db/postgreSQL/seedReservationsAndUsers.js
+++ db/postgreSQL/seedReservationsAndUsers.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const faker = require('faker');
+const cliProgress = require('cli-progress');
 
 const writer = csvWriter();
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 const randRange = (min, max) => Math.floor((Math.random() * (max - min)) + min);
 
 const seedReservationsAndUsers = () => {
   writer.pipe(fs.createWriteStream('reservationsAndUsersData.csv'));
+  bar.start(10000000, 0);
 
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 10000000; i++) {
     for (let j = 0; j < Math.floor((Math.random() * 5) + 5); j++) {
       writer.write({
         reservation_id: j,
@@ -24,9 +27,24 @@ const seedReservationsAndUsers = () => {
         email: faker.internet.email(),
       });
     }
+    bar.update(i);
   }
+  bar.stop();
   writer.end();
   console.log('???? csv writing who knows FOR RESERVATIONS???');
 };
 
 seedReservationsAndUsers();
+
+//csv writer
+
+//native javascript
+
+//could be writing a lot of times
+//faker functions super expensive
+//memory management
+//overload harddisc if dont have space
+// under an hour or two
+// decrease amount of writing to file
+// write in bulk
+// increase speed
