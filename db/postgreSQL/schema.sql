406f5ca874e97498ecf2a2534d31b4b8ed5fea5e
Joanna Wang <shiju.an362@gmail.com>
2020-04-08 00:58:22 -0700

    create pg connection

--- db/postgreSQL/schema.sql
+++ db/postgreSQL/schema.sql
@@ -1,40 +1,35 @@
-DROP DATABASE IF EXISTS relaxlyBypj3;
+DROP DATABASE IF EXISTS relaxly_pj3;
 
-CREATE DATABASE relaxlyBypj3;
+CREATE DATABASE relaxly_pj3;
 
-USE relaxlyBypj3;
+\c relaxly_pj3;
 
 CREATE TABLE users (
-  user_id INT NOT NULL AUTO_INCREMENT,
+  user_id SERIAL PRIMARY KEY,
   username VARCHAR (60),
   email VARCHAR (300)
-)
+);
 
 CREATE TABLE locations (
-   location_id serial PRIMARY KEY,
-   location_address VARCHAR (300),
-   rate int NOT NULL,
-   review_avg NUMERIC(3,2) NOT NULL,
-   total_review int,
-   service_fee int,
-   occupancy_tax int
+  location_id SERIAL PRIMARY KEY,
+  location_address VARCHAR (300),
+  rate INT NOT NULL,
+  review_avg NUMERIC(3,2) NOT NULL,
+  total_review INT,
+  service_fee INT,
+  occupancy_tax INT
 );
 
 CREATE TABLE reservations (
-   reservation_id serial PRIMARY KEY,
-   checkin_date DATE NOT NULL,
-   checkout_date DATE NOT NULL,
-   adults int,
-   children int,
-   infants int,
-   price int,
-   location_id int,
-   user_id int,
-   PRIMARY KEY (reservation_id, location_id)
-   CONSTRAINT reservations_location_id_fkey FOREIGN KEY (location_id)
-      REFERENCES locations (location_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-   CONSTRAINT reservations_user_id_fkey FOREIGN KEY (user_id)
-      REFERENCES users (user_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+  reservation_id SERIAL PRIMARY KEY,
+  checkin_date DATE NOT NULL,
+  checkout_date DATE NOT NULL,
+  adults INT,
+  children INT,
+  infants INT,
+  price INT,
+  location_id INT,
+  user_id INT,
+  FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION,
+  FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
