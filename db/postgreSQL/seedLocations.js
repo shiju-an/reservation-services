af674afc0e67e20fc2d2e960629cb663aec6c0b1
Joanna Wang <shiju.an362@gmail.com>
2020-04-08 09:25:55 -0700

    clean up code

--- db/postgreSQL/seedLocations.js
+++ db/postgreSQL/seedLocations.js
@@ -1,96 +1,3 @@
-// const fs = require('fs');
-// const csvWriter = require('csv-write-stream');
-// const faker = require('faker');
-// const cliProgress = require('cli-progress');
-
-// const writer = csvWriter();
-// const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
-// const randRange = (min, max) => Math.floor((Math.random() * (max - min)) + min);
-
-// const seedLocations = () => {
-//   writer.pipe(fs.createWriteStream('locationsData.csv'));
-
-//   const rate = randRange(20, 100);
-//   const review_avg = (Math.random() * 5).toFixed(2);
-//   const total_review = randRange(50, 500);
-//   const serviceFee = randRange(5, 10);
-//   const occupancyTax = randRange(1, 8);
-
-//   const total = 10000000;
-
-//   bar.start(total, 0);
-
-//   for (let i = 0; i < total; i++) {
-//     writer.write({
-//       location_id: i,
-//       location_address: faker.fake('{{address.streetAddress}}, {{address.city}}, {{address.state}}'),
-//       rate,
-//       reviewAvg,
-//       totalReview,
-//       serviceFee,
-//       occupancyTax,
-//     });
-//     bar.update(i);
-//   }
-//   bar.stop();
-//   console.log('???? COME IN CSV HAVE YOU SEEDED???');
-//   writer.end();
-// };
-
-// seedLocations();
-
-
-// const fs = require('fs');
-// const faker = require('faker');
-// const cliProgress = require('cli-progress');
-
-// const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
-// const writeLocations = fs.createWriteStream('pgLocations.csv');
-// writeLocations.write('id, location_address, rate, reviewAvg, totalReview, serviceFee, occupancyTax\n', 'utf8');
-
-// function writeTenMillionLocations (writer, encoding, callback) {
-//   const randRange = (min, max) => Math.floor((Math.random() * (max - min)) + min);
-
-//   let i = 10000000;
-//   let id = 0;
-
-//   bar.start(i, 0);
-
-//   function write() {
-//     let ok = true;
-//     do {
-//       i -= 1;
-//       id += 1;
-//       let location_address = faker.fake('{{address.streetAddress}}, {{address.city}}, {{address.state}}');
-//       let rate = randRange(20, 100);
-//       let reviewAvg = (Math.random() * 5).toFixed(2);
-//       let totalReview = randRange(50, 500);
-//       let serviceFee = randRange(5, 10);
-//       let occupancyTax = randRange(1, 8);
-//       const data = `${id}, ${rate}, ${reviewAvg}, ${totalReview}, ${serviceFee}, ${occupancyTax}\n`;
-//       if (i = 0) {
-//         writer.write(data, encoding, callback);
-//       } else {
-//         bar.update(i);
-//         ok = writer.write(data, encoding);
-//       }
-//     } while (i > 0 && ok);
-
-//     if (i > 0) {
-//       writer.once('drain', write);
-//     }
-//   }
-//   write();
-// }
-
-// writeTenMillionLocations(writeLocations, 'utf-8', () => {
-//   bar.stop();
-//   writeLocations.end();
-// });
-
-
 const faker = require('faker');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const cliProgress = require('cli-progress');
@@ -161,3 +68,52 @@ const writeLocations = () => {
 
 bar.start(uniqueTotal * total, 0);
 writeLocations();
+
+// const fs = require('fs');
+// const faker = require('faker');
+// const cliProgress = require('cli-progress');
+
+// const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+// const writeLocations = fs.createWriteStream('pgLocations.csv');
+// writeLocations.write('id, location_address, rate, reviewAvg, totalReview, serviceFee, occupancyTax\n', 'utf8');
+
+// function writeTenMillionLocations (writer, encoding, callback) {
+//   const randRange = (min, max) => Math.floor((Math.random() * (max - min)) + min);
+
+//   let i = 10000000;
+//   let id = 0;
+
+//   bar.start(i, 0);
+
+//   function write() {
+//     let ok = true;
+//     do {
+//       i -= 1;
+//       id += 1;
+//       let location_address = faker.fake('{{address.streetAddress}}, {{address.city}}, {{address.state}}');
+//       let rate = randRange(20, 100);
+//       let reviewAvg = (Math.random() * 5).toFixed(2);
+//       let totalReview = randRange(50, 500);
+//       let serviceFee = randRange(5, 10);
+//       let occupancyTax = randRange(1, 8);
+//       const data = `${id}, ${rate}, ${reviewAvg}, ${totalReview}, ${serviceFee}, ${occupancyTax}\n`;
+//       if (i = 0) {
+//         writer.write(data, encoding, callback);
+//       } else {
+//         bar.update(i);
+//         ok = writer.write(data, encoding);
+//       }
+//     } while (i > 0 && ok);
+
+//     if (i > 0) {
+//       writer.once('drain', write);
+//     }
+//   }
+//   write();
+// }
+
+// writeTenMillionLocations(writeLocations, 'utf-8', () => {
+//   bar.stop();
+//   writeLocations.end();
+// });
--- db/postgreSQL/seedReservationsAndUsers.js
+++ db/postgreSQL/seedReservationsAndUsers.js
@@ -1,55 +1,3 @@
-// const fs = require('fs');
-// const csvWriter = require('csv-write-stream');
-// const faker = require('faker');
-// const cliProgress = require('cli-progress');
-
-// const writer = csvWriter();
-// const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
-// const randRange = (min, max) => Math.floor((Math.random() * (max - min)) + min);
-
-// const seedReservationsAndUsers = () => {
-//   writer.pipe(fs.createWriteStream('reservationsAndUsersData.csv'));
-//   bar.start(20, 0);
-
-//   for (let i = 0; i < 20; i++) {
-//     for (let j = 0; j < Math.floor((Math.random() * 5) + 5); j++) {
-//       writer.write({
-//         id: i,
-//         reservation_id: j,
-//         checkin: `2020-${j + 1}-${randRange(1, 14)}`,
-//         checkout: `2020-${j + 1}-${randRange(15, 28)}`,
-//         adults: randRange(1, 5),
-//         children: randRange(0, 5),
-//         infants: randRange(0, 5),
-//         price: randRange(100, 2000),
-//         user_id: j,
-//         username: faker.internet.userName(),
-//         email: faker.internet.email(),
-//       });
-//     }
-//     bar.update(i);
-//   }
-//   bar.stop();
-//   writer.end();
-//   console.log('???? csv writing who knows FOR RESERVATIONS???');
-// };
-
-// seedReservationsAndUsers();
-
-//csv writer
-
-//native javascript
-
-//could be writing a lot of times
-//faker functions super expensive
-//memory management
-//overload harddisc if dont have space
-// under an hour or two
-// decrease amount of writing to file
-// write in bulk
-// increase speed
-
 const faker = require('faker');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const cliProgress = require('cli-progress');
@@ -169,3 +117,16 @@ const writeUsers = () => {
 
 bar1.start(total, 0);
 writeReservations();
+
+//csv writer
+
+//native javascript
+
+//could be writing a lot of times
+//faker functions super expensive
+//memory management
+//overload harddisc if dont have space
+// under an hour or two
+// decrease amount of writing to file
+// write in bulk
+// increase speed
